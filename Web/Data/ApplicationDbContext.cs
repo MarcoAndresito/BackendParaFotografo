f2b6d31f57ace2379d44906ae20921a53e6ea268
Andres Vasquez <ken.andr3s@gmail.com>
2025-04-10 21:24:18 -0400

    configurando los paquetes y agregando la primera migracion

---
+++ Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web.Models;
+
+namespace Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Producto>(producto =>
+            {
+                producto
+                .HasKey((p) => p.Id);
+
+                producto
+                .Property((p) => p.Nombre)
+                .IsRequired()
+                .HasMaxLength(50);
+
+                producto
+                .Property((p) => p.Precio)
+                .IsRequired()
+                .HasPrecision(9, 2);
+            });
+        }
+    }
+}
