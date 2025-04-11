5da95b6964f1aeacf7daa4318eea01a89ca70d86
Andres Vasquez <ken.andr3s@gmail.com>
2025-04-10 21:48:25 -0400

    segunda mirgacion

--- Web/Models/Producto.cs
+++ Web/Models/Producto.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
+        public string Marca { get; set; }
         public decimal Precio { get; set; }
         public int Stock { get; set; }
     }
