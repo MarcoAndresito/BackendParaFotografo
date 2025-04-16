d4a2b60658a30e02f78323352531d83d018fec93
icv-9 <154081131+icv-9@users.noreply.github.com>
2025-04-16 13:20:16 -0400

    mi proyecto subir foto album

---
+++ Web/Models/Foto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models { 
+    public class Foto
+    {
+        public int Id { get; set; }
+        public int AlbumId { get; set; }
+        public string NombreArchivo { get; set; }
+        public string Url { get; set; }
+        public string Formato { get; set; }
+        public int PesoKB { get; set; }
+        public int AnchoPx { get; set; }
+        public int AltoPx { get; set; }
+        public string PublicId { get; set; }
+        public DateTime FechaSubida { get; set; }
+        public Album Album { get; set; }
+    }
+}
