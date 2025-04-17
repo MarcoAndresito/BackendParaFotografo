3a524d6c732ece1ffbda8e01f82ab6cba1484d03
Yanila <solizyanila23@gmail.com>
2025-04-16 20:01:51 -0400

    Agregación de directivas de uso en el controlador

--- Web/Controllers/ExportarAlbumController.cs
+++ Web/Controllers/ExportarAlbumController.cs
@@ -1,6 +1,9 @@
 ﻿// YANILA SOLIZ DURAN
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Web.Data;
 using Web.Models;
 
