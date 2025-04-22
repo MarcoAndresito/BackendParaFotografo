91604bf30e1a3f81ab692b5dad32a10a3178ccea
Jeffreyvf <jeffreyvaldiviaflores@gmail.com>
2025-04-22 11:46:48 -0400

    Se implemento required al registro de usuario.cs

--- Web/Models/RegistroUsuario.cs
+++ Web/Models/RegistroUsuario.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Web.Models
 {
     public class RegistroUsuario
     {
         public int Id { get; set; }
+        [Required]
         public string Nombre { get; set; }
+        [Required]
         public string Correo { get; set; }
+        [Required]
         public string Contraseña { get; set; }
     }
 }
