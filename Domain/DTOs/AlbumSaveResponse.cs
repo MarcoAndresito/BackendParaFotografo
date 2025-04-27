e8f14608938e98bea97626121b68afdd7f54e97a
Yanila <solizyanila23@gmail.com>
2025-04-27 17:41:28 -0400

    se corrio los errores de la rama main -> exportar

--- Domain/DTOs/AlbumSaveResponse.cs
+++ Domain/DTOs/AlbumSaveResponse.cs
@@ -6,4 +6,9 @@ public class AlbumSaveResponse
 {
     public string mensage { get; set; }
     public Album album { get; set; }
+    public string Messague { get; set; } = string.Empty;
+    public string FilePath { get; set;} = string.Empty;
+
+
+    
 }
--- Domain/DTOs/ExportarAlbumResponce.cs
+++ Domain/DTOs/ExportarAlbumResponce.cs
@@ -2,6 +2,9 @@
 
 public class ExportarAlbumResponce
 {
+    public string Message;
+    public string FilePath;
+
     FileStream archivComprimido { get; set; }
     byte[] archivComprimido2 { get; set; }
     string archivComprimido3 { get; set; }
